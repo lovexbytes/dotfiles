a250d517f64e3aae0042701a39ff4ee01093dacb
lovexbytes <lovexbytes@proton.me>
2024-06-06 04:39:58 +0300

    modified git neotree integration

--- nvim/after/plugin/neotree.lua
+++ nvim/after/plugin/neotree.lua
@@ -3,6 +3,24 @@ vim.opt.termguicolors = true
 vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle position=right<CR>")
 
 require("neo-tree").setup({
+	default_component_configs = {
+		git_status = {
+			symbols = {
+				-- Change type
+				added = "+", -- NOTE: you can set any of these to an empty string to not show them
+				deleted = "✕",
+				modified = "~",
+				renamed = "~",
+				-- Status type
+				untracked = "",
+				ignored = "",
+				unstaged = "",
+				staged = "",
+				conflict = "",
+			},
+			align = "right",
+		},
+	},
 	close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
 	hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree in window.position
 	window = {
