14d03c01eba9a3af6a592816504d933fe4e8abb0
lovexbytes <lovexbytes@proton.me>
2026-03-19 18:03:32 +0200

    nvim upd

--- nvim/lua/lovexbytes/packer.lua
+++ nvim/lua/lovexbytes/packer.lua
@@ -19,7 +19,15 @@ return require("lazy").setup({
 	-- telescope
 	{ "nvim-lua/plenary.nvim" },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.5" },
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- optional but recommended
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+	},
 	-- colorscheme
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	-- { "datsfilipe/vesper.nvim" },
